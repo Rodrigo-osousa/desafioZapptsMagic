0c517b6cd6483793d9911f822319fe2e2cf1e45c
Rodrigo Oliveira Sousa <79177544+Rodrigo-osousa@users.noreply.github.com>
2022-05-03 15:50:14 -0300

    Adicionado Cartas e Swagguer no projeto

--- src/main/java/com/zappts/magic/models/Card.java
+++ src/main/java/com/zappts/magic/models/Card.java
@@ -11,5 +11,80 @@ public class Card {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer CardId;
     private String cardName;
-    private
+    private String cardEdition;
+    private String cardLanguage;
+    private Boolean cardFoil;
+    private Double cardAmount;
+
+    public Card() {
+    }
+
+    public Card(Integer cardId, String cardName, String cardEdition, String cardLanguage, Boolean cardFoil, Double cardAmount) {
+        CardId = cardId;
+        this.cardName = cardName;
+        this.cardEdition = cardEdition;
+        this.cardLanguage = cardLanguage;
+        this.cardFoil = cardFoil;
+        this.cardAmount = cardAmount;
+    }
+
+    public Integer getCardId() {
+        return CardId;
+    }
+
+    public void setCardId(Integer cardId) {
+        CardId = cardId;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public String getCardEdition() {
+        return cardEdition;
+    }
+
+    public void setCardEdition(String cardEdition) {
+        this.cardEdition = cardEdition;
+    }
+
+    public String getCardLanguage() {
+        return cardLanguage;
+    }
+
+    public void setCardLanguage(String cardLanguage) {
+        this.cardLanguage = cardLanguage;
+    }
+
+    public Boolean getCardFoil() {
+        return cardFoil;
+    }
+
+    public void setCardFoil(Boolean cardFoil) {
+        this.cardFoil = cardFoil;
+    }
+
+    public Double getCardAmount() {
+        return cardAmount;
+    }
+
+    public void setCardAmount(Double cardAmount) {
+        this.cardAmount = cardAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "CardId=" + CardId +
+                ", cardName='" + cardName + '\'' +
+                ", cardEdition='" + cardEdition + '\'' +
+                ", cardLanguage='" + cardLanguage + '\'' +
+                ", cardFoil=" + cardFoil +
+                ", cardAmount=" + cardAmount +
+                '}';
+    }
 }
